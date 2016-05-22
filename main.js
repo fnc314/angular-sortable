3ada48ea3bdf50fcdc46a00ab3c795ce3f2f6e6d
Franco N. Colaizzi <fnc314@gmail.com>
2016-05-22 14:56:54 -0400

    working on main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+  Package Name: Angular sorTable Module
+  Version: 1.0.0
+  Author: Franco N. Colaizzi (fnc314@gmail.com)
+  License: MIT
+  Repository: https://github.com/fnc314/angular-sortable
+  Copyright: 2016
+*/
+
+var sorTable = angular.module('sorTable', []);
+
+// Wrapper directive
+sorTable.directive('sortableTable')
